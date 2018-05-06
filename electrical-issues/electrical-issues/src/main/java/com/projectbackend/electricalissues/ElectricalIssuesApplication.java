4c1062169035948c60ca94fd8620b1eeb34b6ce4
OLIVER MENSAH <you@example.com>
2018-05-06 09:56:08 -0700

    backend service for electrical issue report

---
+++ electrical-issues/electrical-issues/src/main/java/com/projectbackend/electricalissues/ElectricalIssuesApplication.java
@@ -0,0 +1,12 @@
+package com.projectbackend.electricalissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricalIssuesApplication.class, args);
+	}
+}
