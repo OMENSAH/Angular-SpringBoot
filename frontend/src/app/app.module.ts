6bdcc989f8fb7497a1247813beca9eba42373ad9
OLIVER MENSAH <you@example.com>
2018-05-08 09:07:30 -0700

    added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
 import { AppComponent } from './app.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-
 
 @NgModule({
   declarations: [
