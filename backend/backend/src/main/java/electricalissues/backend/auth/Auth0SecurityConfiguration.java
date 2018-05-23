d2af023d6868def2e07dac488cde24fccf97839e
OLIVER MENSAH <you@example.com>
2018-05-22 21:07:57 -0700

    added

--- backend/backend/src/main/java/electricalissues/backend/auth/Auth0SecurityConfiguration.java
+++ backend/backend/src/main/java/electricalissues/backend/auth/Auth0SecurityConfiguration.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package electricalissues.backend.config;
+package electricalissues.backend.auth;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
